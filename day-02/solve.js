9d280fafa6629bf68b44214d1379c3fe1abb1f9a
adamgibbons <adam.d.gibbons@gmail.com>
2017-12-05 19:47:47 -0700

    solve day 2

--- day-02/solve.js
+++ day-02/solve.js
@@ -1,17 +1,39 @@
-// part 1
+const { readFileSync } = require('fs')
 
-const fs = require('fs')
-
-const result = fs.readFileSync('./data.csv')
+const massagedInput = readFileSync('./data.csv')
   .toString()
   .split("\n")
   .map(row => row.split("\t")
     .map(i => parseInt(i)))
-  .map((row) => {
-    return Math.max(...row) - Math.min(...row)
-  })
-  .reduce((a, b) => {
-    return a + b
-  })
 
-console.log(result)
+function solvePartOne (data) {
+  return data.map((row) => {
+    return Math.max(...row) - Math.min(...row)
+  }).reduce((a, b) => { return a + b })
+}
+
+function solvePartTwo (data) {
+  return data.map((row) => {
+    return row.sort((b, a) => {
+      if (a < b) return -1
+      if (a > b) return 1
+      return 0
+    }).reduce((accumulator, currentValue, idx, subject) => {
+      const result = (subject.slice(idx + 1).find((i) => currentValue % i === 0))
+
+      if (result) {
+        accumulator += (currentValue / result)
+        return accumulator
+      }
+
+      return accumulator
+    }, 0)
+  }).reduce((a, b) => a + b)
+}
+
+function solve (data) {
+  console.log("Part One: ", solvePartOne(data))
+  console.log("Part Two: ", solvePartTwo(data))
+}
+
+solve(massagedInput)
