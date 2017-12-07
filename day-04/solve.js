20af120e20c8c5a3693d9bdc8fb4f12677bd66ce
adamgibbons <adam.d.gibbons@gmail.com>
2017-12-06 21:57:38 -0700

    solve day four part 2

--- day-04/solve.js
+++ day-04/solve.js
@@ -9,24 +9,47 @@ function rest(list, position) {
   return list.slice(position + 1)
 }
 
-const validPhrases = phrases.map((phrase) => {
-  return phrase.reduce((accumulator, currentValue, position, subject) => {
+function solvePartOne(phrases) {
+  return phrases.map((phrase) => {
+    return phrase.reduce((accumulator, currentValue, position, subject) => {
+      if (rest(subject, position).length === 0) return accumulator
 
-    if (rest(subject, position).length === 0) { return accumulator }
-
-    if (rest(subject, position).indexOf(currentValue) !== -1) {
-      const idx = rest(subject, position).indexOf(currentValue)
-      const matchedWord = rest(subject, position)[idx]
-      if (matchedWord.length === currentValue.length) {
-        accumulator.push(currentValue)
-        return accumulator
+      if (rest(subject, position).indexOf(currentValue) !== -1) {
+        const idx = rest(subject, position).indexOf(currentValue)
+        const matchedWord = rest(subject, position)[idx]
+        if (matchedWord.length === currentValue.length) {
+          accumulator.push(currentValue)
+          return accumulator
+        }
       }
-    }
 
-    return accumulator
+      return accumulator
+    }, [])
+  }).filter(words => words.length === 0).length
+}
 
-  }, [])
-}).filter(words => words.length === 0)
+function solvePartTwo(phrases) {
+  return phrases.map((phrase) => {
+    return phrase
+      .map((words) => {
+        return words.split("").sort().join("")
+      })
+      .reduce((accumulator, currentValue, position, subject) => {
+        if (rest(subject, position).length === 0) return accumulator
 
-console.log(validPhrases.length)
+        if (rest(subject, position).indexOf(currentValue) !== -1) {
+          const idx = rest(subject, position).indexOf(currentValue)
+          const matchedWord = rest(subject, position)[idx]
+          if (matchedWord.length === currentValue.length) {
+            accumulator.push(currentValue)
+            return accumulator
+          }
+        }
 
+        return accumulator
+      }, [])
+  }).filter(line => line.length === 0).length
+}
+
+console.log(solvePartOne(phrases))
+console.log(solvePartTwo(phrases))
