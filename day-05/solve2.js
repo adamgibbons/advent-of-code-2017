0e20dcd25d2e759e19b8b29c97570f4feb733aa7
adamgibbons <adam.d.gibbons@gmail.com>
2017-12-10 09:23:31 -0700

    wip: day five part two

--- day-05/solve2.js
+++ day-05/solve2.js
@@ -1,5 +1,5 @@
 const { readFileSync } = require('fs')
-const list = readFileSync('./data.txt').toString().split('\n').map(i => parseInt(i))
+const rawList = readFileSync('./test-data.txt').toString().split('\n').map(i => parseInt(i))
 
 function solvePart1 (rawList) {
   var list = rawList.concat([])
@@ -19,4 +19,33 @@ function solvePart1 (rawList) {
   return counter
 }
 
-console.log(`Part 1: ${solvePart1(list)}`)
+function solvePart2(rawList) {
+  var list = rawList.concat([])
+  var counter = 0
+  var position = 0
+
+  do {
+
+    console.log(list)
+
+    var nextPosition = position + list[position]
+    var offset = Math.abs(position - list[position])
+
+    if (offset > 2)  {
+      list[position] = list[position] - 1
+    } else {
+      list[position] = list[position] + 1
+    }
+
+    // var nextPosition = position + list[position]
+    position = nextPosition
+
+    counter++
+    // console.log(position)
+  } while (list[position] !== undefined)
+
+  return counter
+}
+
+// console.log(`Part 1: ${solvePart1(list)}`)
+console.log(`Part 2: ${solvePart2(rawList)}`)
